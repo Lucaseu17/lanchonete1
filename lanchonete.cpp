2ace9e3986f1685da29895767f875e44686d9cb4
Lucaseu17 <54222833+Lucaseu17@users.noreply.github.com>
2019-08-19 17:06:13 -0300

    nab

--- lanchonete.cpp
+++ lanchonete.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
 struct estruturaDoProduto
 {
     char nomeDoProduto[50];
@@ -11,17 +13,18 @@ struct estruturaDoProduto
     */
 } produto[1000];
 
-struct estruturaDoPedido
-{
-    int idPedido;
-    int produtosPedidos[100];
-    float total;
-    /*
-    int dia, mes, ano;
-    */
-} pedido[1000];
 
-using namespace std;
+//struct estruturaDaComanda
+//{
+//    int idPedido;
+//    struct estruturaDoProduto;
+//    float total;
+//    /*
+//    int dia, mes, ano;
+//    */
+//} comanda[1000];
+
+
 
 bool ProcuraPedido(int numPed, char nomePed[50], float &preco)
 {
@@ -50,45 +53,90 @@ bool ProcuraPedido(int numPed, char nomePed[50], float &preco)
     fclose(arquivo);
     return false;
 }
+//void mostrarEstoque{
+//
+// FILE *arquivo;
+//
+//    arquivo = fopen("Estoque.txt", "r");
+//
+//
+//
+//}
 
-//void faturamentoDiario()
+
+void alteraEstoque(int numProd, int quantidade){
+
+ FILE *arquivo;
+
+    arquivo = fopen("Cardapio.txt", "r");
+
+
+
+}
 
 int main()
 {
     char nomePed[50];
     float preco, faturamentTot=0;
     int pedido, dia = 1, i=1, j;
+    int numCom;
+
+
 
     while (dia != 0)
     {
+//        cout << "Digite o numero da comanda: "
+//        cin >> numCom;
+//
         cout << "Digite o numero do pedido: "
              << endl;
 
         cin >> pedido;
 
+
         if (ProcuraPedido(pedido, nomePed, preco))
         {
             cout << "PEDIDO: " << nomePed << "\n";
-            cout << "Preco: " << preco << "\n";
+            cout << "Preco: R$" << preco << "\n";
         }
+
+
 
         produto[i].idProduto = pedido;
         strcpy(produto[i].nomeDoProduto,nomePed);
         produto[i].preco = preco;
         i++;
         faturamentTot += preco;
+
+
         cout << "Finalizar dia? [1]Nao [0]Sim" << endl;
         cin >> dia;
     }
 
+    FILE *arquivo2;
 
+    arquivo2 = fopen("faturamentoDiario.txt", "w");
 
+    fprintf(arquivo2,"Lista de vendas diaria: \n");
 
     cout << "Lista de vendas diaria:"
          << "\n";
+
     for(j=1; j<i; j++){
-    cout << produto[j].idProduto << "  " <<produto[j].nomeDoProduto << "  " << produto[j].preco << endl;
+    cout << produto[j].idProduto << "  " <<produto[j].nomeDoProduto << "  R$" << produto[j].preco << endl;
+
+    fprintf(arquivo2,"%d",produto[j].idProduto);
+    fprintf(arquivo2,"  ");
+    fprintf(arquivo2,"%s",produto[j].nomeDoProduto);
+    fprintf(arquivo2,"  R$");
+    fprintf(arquivo2,"%.2f",produto[j].preco);
+    fprintf(arquivo2,"\n");
     }
-    cout << "faturamento do dia: " << faturamentTot <<endl;
+    cout << "faturamento do dia: R$" << faturamentTot <<endl;
+
+    fprintf(arquivo2,"faturamento do dia: R$");
+    fprintf(arquivo2,"%.2f",faturamentTot);
+
+
     return 0;
 }
