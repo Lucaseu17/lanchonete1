f274fc91b5f8c3900f79354cb34e3e1192931c42
Rafael <rrvsrafael@gmail.com>
2019-08-19 13:37:42 -0300

    structs novas

--- lanchonete.cpp
+++ lanchonete.cpp
@@ -1,5 +1,25 @@
 #include <bits/stdc++.h>
-//PELEZINHO SEU VIADO
+
+struct estruturaDoProduto
+{
+    char nomeDoProduto[50];
+    int idProduto;
+    float preco;
+    /*
+    int qtdEstoque;
+    float precoDeCusto;
+    */
+} produto[1000];
+
+struct estruturaDoPedido
+{
+    int idPedido;
+    int produtosPedidos[100];
+    float total;
+    /*
+    int dia, mes, ano;
+    */
+} pedido[1000];
 
 using namespace std;
 
@@ -32,42 +52,31 @@ bool ProcuraPedido(int numPed, char nomePed[50], float &preco)
 
 //void faturamentoDiario()
 
-
 int main()
 {
     char nomePed[50];
     float preco;
-    int pedido, dia=1;
-    struct listaDia{
-        int numero;
-        char pedido;
-        float preco;
-    };
+    int pedido, dia = 1;
 
-
-    while(dia != 0){
-    cout << "Digite o numero do pedido: "
-         << endl;
-
-    cin >> pedido;
-
-
-    if (ProcuraPedido(pedido, nomePed, preco))
+    while (dia != 0)
     {
-        cout << "PEDIDO: " << nomePed << "\n";
-        cout << "Preco: " << preco << "\n";
+        cout << "Digite o numero do pedido: "
+             << endl;
+
+        cin >> pedido;
+
+        if (ProcuraPedido(pedido, nomePed, preco))
+        {
+            cout << "PEDIDO: " << nomePed << "\n";
+            cout << "Preco: " << preco << "\n";
+        }
+
+        cout << "Finalizar dia? [1]Nao [0]Sim" << endl;
+        cin >> dia;
     }
 
-
-
-
-    cout <<"Finalizar dia? [1]Nao [0]Sim"<<endl;
-    cin >> dia;
-    }
-
-    cout << "Lista de vendas diaria:" << "\n";
-
-
+    cout << "Lista de vendas diaria:"
+         << "\n";
 
     return 0;
 }
