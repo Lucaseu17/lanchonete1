4991a066d7661848c55ea67edf1fc6093d6efb89
Lucaseu17 <54222833+Lucaseu17@users.noreply.github.com>
2019-08-19 22:46:16 -0300

    nabo

--- lanchonete.cpp
+++ lanchonete.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using namespace std::chrono;
 
 struct estruturaDoProduto
 {
@@ -63,31 +64,47 @@ void mostrarEstoque(){
  FILE *arquivo;
 
     arquivo = fopen("Estoque.txt", "r");
-    cout << "id" <<"  "<<"produto"<<"  "<<"preco"<<"  "<<"qntd"<<endl;
+    cout <<setw(3) << left << "id" <<  "  "<<
+           setw(15)<< left << "produto"<< "  " <<
+           setw(15)<< left << "preco de Custo" << "  " <<
+           setw(10)<< left << "quantidade" << "\n" << endl;
     while(!feof(arquivo)){
     fscanf(arquivo,"%d %s %f %d",&id, &prod, &precoDeCusto, &quant);
 
-    cout << id <<"  "<< prod <<"  " << precoDeCusto <<"  " << quant<<endl;
+    cout << setw(3)<< left << id << "  " <<
+           setw(15)<< left << prod << "  R$" <<
+           setw(15)<< left << precoDeCusto << "  " <<
+           setw(10)<< left << quant << endl;
     }
 }
 
 
-void alteraEstoque(int numProd, int quantidade){
+void alteraEstoque(int numProd, int uni){
+    int id, quant, q;
+    char prod[50];
+    float precoDeCusto;
 
  FILE *arquivo;
 
-    arquivo = fopen("Cardapio.txt", "r");
+    arquivo = fopen("Estoque.txt", "a");
 
+    {
+        fscanf(arquivo,"%d %s %f %d",&id, &prod, &precoDeCusto, &quant);
 
+            if(id==numProd){
+                q = quant - uni;
+                fprintf(arquivo,"%d",q);
+            }
+    }
 
 }
 
 int main()
 {
     char nomePed[50];
-    float preco, faturamentTot=0;
-    int pedido, sim = 1, i=1, j;
-    int numCom;
+    float preco, faturamentTot=0, precoTot;
+    int pedido, sim = 1, i=1, j, k=1;
+    int numCom, Unid[1000], unidade;
 
 
 
@@ -96,29 +113,37 @@ int main()
 //        cout << "Digite o numero da comanda: "
 //        cin >> numCom;
 //
-        cout << "Digite o numero do pedido: "
-             << endl;
-
+        cout << "Digite o numero do pedido: ";
         cin >> pedido;
-
+        cout <<"\n";
 
         if (ProcuraPedido(pedido, nomePed, preco))
         {
-            cout << "PEDIDO: " << nomePed << "\n";
-            cout << "Preco: R$" << preco << "\n";
+            cout << "PEDIDO: " << nomePed << "\n\n";
+            cout << "Numero de Unidades: ";
+            cin >> unidade;
+            Unid[k] = unidade;
+            k++;
+            precoTot = preco*unidade;
+            cout << "\n\n" << "Preco: R$" << precoTot << "\n\n";
+//            if(pedido <= 5)
+                alteraEstoque(pedido, unidade);
+        }else{
+            cout <<"Pedido nao encontrado \n" <<endl;
         }
 
 
 
         produto[i].idProduto = pedido;
         strcpy(produto[i].nomeDoProduto,nomePed);
-        produto[i].preco = preco;
+        produto[i].preco = precoTot;
         i++;
-        faturamentTot += preco;
+        faturamentTot += precoTot;
 
 
-        cout << "Finalizar dia? [1]Nao [0]Sim" << endl;
+        cout << "Finalizar dia?   [1]Nao [0]Sim\n" << endl;
         cin >> sim;
+        cout <<"\n";
     }
 
     FILE *arquivo2;
@@ -128,10 +153,23 @@ int main()
     fprintf(arquivo2,"Lista de vendas diaria: \n");
 
     cout << "Lista de vendas diaria:"
-         << "\n";
+         << "\n\n";
+
+//    time_t rawtime;
+//    struct tm * timeinfo;
+//
+//    time ( &rawtime );
+//    timeinfo = localtime ( &rawtime );
+//    printf ( "Data atual do sistema é: %s", asctime (timeinfo) );
+    auto now = system_clock::now();
+    time_t t = system_clock::to_time_t(now); // poderia ser auto t = ...
+
 
     for(j=1; j<i; j++){
-    cout << produto[j].idProduto << "  " <<produto[j].nomeDoProduto << "  R$" << produto[j].preco << endl;
+    cout <<setw(4)<<left<< put_time(localtime(&t), "%c") << "  " <<
+           setw(17)<<left<< produto[j].nomeDoProduto <<
+           setw(3)<<left<< Unid[j]<< "uni" <<"  "<<
+           setw(4) << left << "  R$" << produto[j].preco << endl;
 
     fprintf(arquivo2,"%d",produto[j].idProduto);
     fprintf(arquivo2,"  ");
@@ -140,13 +178,15 @@ int main()
     fprintf(arquivo2,"%.2f",produto[j].preco);
     fprintf(arquivo2,"\n");
     }
-    cout << "faturamento do dia: R$" << faturamentTot <<endl;
+
+    cout <<"\n"<<setw(48)<< "faturamento do dia: " << "R$" << faturamentTot << "\n" << endl;
 
     fprintf(arquivo2,"faturamento do dia: R$");
     fprintf(arquivo2,"%.2f",faturamentTot);
 
-    cout << "Mostrar estoque?     [1]Sim [0]Nao"<<endl;
+    cout << "Mostrar estoque?     [1]Sim [0]Nao" <<endl;
     cin >> sim;
+    cout << "\n";
 
     if(sim == 1){
         mostrarEstoque();
